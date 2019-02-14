1fceb04963bac73a9895ebfcd5d26bcb23798ab8
mandlebrot <l.lautenschlager@gmail.com>
2019-02-14 11:04:49 -0700

    edited 1DL1 to run independently

--- 1DL1min.py
+++ 1DL1min.py
@@ -1,7 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
-import os
+import random as rd
+
+n=10
+h=1./n
+eps = 0.1
+
+def Bds(n): #artificial data generator
+    Up = np.zeros(n)
+    Lo = np.zeros(n)
+    rand = np.zeros(n)
+    x = np.zeros(n)
+    s = np.zeros(n)
+    for i in range(n):
+        x[i] = i*h
+        s[i] = abs(.5-x[i])
+        rand[i] = rd.random()
+        if rand[i] > 0.5:
+            Up[i] = s[i] - rand[i]*eps
+            Lo[i] = s[i] - rand[i]*eps
+        else:
+            Up[i] = -1
+            Lo[i] = -1
+    return Up, Lo
+
+#test stuff
+Up,Lo = Bds(n)
+print(Up)
+print(Lo)
+print(h**2)
 
 # Use Pyomo
 from pyomo.environ import *
