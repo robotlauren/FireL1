f0add41c977f9e4ea48f867daa2187d269385a42
mandlebrot <l.lautenschlager@gmail.com>
2019-04-08 11:13:21 -0600

    add absolute values (for L1)

--- syntest.py
+++ syntest.py
@@ -96,13 +96,13 @@ def ObjRule(m):
 #     v2_y = (m.v2[i,j+1]-m.v2[i,j])/dy
     sum1 = sum(
         sum(
-            (
+            abs(
                 (m.v1[i,j]-(m.u[i+1,j]-m.u[i,j])/dx)+(
                     m.v2[i,j]-(
                         m.u[i,j+1]-m.u[i,j])/dy))*dx*dy for i in m.VM) for j in m.VN)
     sum2 = sum(
         sum(
-            (
+            abs(
                 ((m.v1[i+1,j]-m.v1[i,j])/dx)+(
                     (m.v1[i,j+1]-m.v1[i,j])/dy+(
                         m.v2[i+1,j]-m.v2[i,j])/dx)/2+(
